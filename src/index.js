b739e723cfe3f550fc16ab1a7e73f083cd60e47e
Phumrapee Limpianchop <git@rayriffy.com>
2019-01-02 19:08:10 +0700

    :sprakles: ESLinted

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import * as functions from "firebase-functions"
-import dotenv from "dotenv"
+import * as functions from 'firebase-functions'
+import dotenv from 'dotenv'
+import rp from 'request-promise'
 
-dotenv.config();
+dotenv.config()
 
 const LINE_API_URL = 'https://api.line.me/v2/bot/message'
 const REQUEST_HEADER = {
-  'Content-Type': 'application/json',
-  'Authorization': `Bearer ` + process.env.LINE_ACCESS_TOKEN
+  'Content-Type': `application/json`,
+  Authorization: `Bearer ` + process.env.LINE_ACCESS_TOKEN,
 }
 
 export let test = functions.https.onRequest((req, res) => {
