1128b57739e9852a166ecf9e7b1a564907afa9c7
Phumrapee Limpianchop <git@rayriffy.com>
2019-01-02 14:00:34 +0700

    :tada: Intitial C

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import * as functions from "firebase-functions"
+import dotenv from "dotenv"
+
+dotenv.config();
+
+const LINE_API_URL = 'https://api.line.me/v2/bot/message'
+const REQUEST_HEADER = {
+  'Content-Type': 'application/json',
+  'Authorization': `Bearer ` + process.env.LINE_ACCESS_TOKEN
+}
+
+export let test = functions.https.onRequest((req, res) => {
+  console.log(req.body)
+  res.status(200).send(`OK`)
+})
